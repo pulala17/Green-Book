011bb6a5463dc59e46db18b18a2af5a9876f967d
Xaria <89126668+pulala17@users.noreply.github.com>
2022-03-18 15:47:21 +0800

    Update 2_data_types.sv

--- 2_data_types.sv
+++ 2_data_types.sv
@@ -6,3 +6,9 @@
 //    • Unions and packed structures: allow multiple views ofthe same data
 //    • Strings: built-in string support
 //    • Enumerated types: code is easier to write and understand
+
+
+// A 'logic' signal can be used anywhere a net is used, 
+// except that a logic variable cannot be driven by [multiple structural drivers], 
+// such as when you are modeling a [bidirectional bus]. 
+// In this case, the variable needs to be a net-type such as 'wire' so that SV can resolve the multiple values to determine the final value.
