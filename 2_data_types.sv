b088eb08b8d7e72e26980042d285c8dcffcdb8bd
Xaria <89126668+pulala17@users.noreply.github.com>
2022-03-18 15:34:13 +0800

    Create 2_data_types.sv

---
+++ 2_data_types.sv
@@ -0,0 +1,8 @@
+//
+// SystemVerilog introduces new data types with the following benefits.
+//    • Two-state: better performance, reduced memory usage
+//    • Queues, dynamic and associative arrays: reduced memory usage, built-in support for searching and sorting
+//    • Classes and structures: support for abstract data structures
+//    • Unions and packed structures: allow multiple views ofthe same data
+//    • Strings: built-in string support
+//    • Enumerated types: code is easier to write and understand
