a3fab36ff5627d593633352e692032575882a6e4
Xaria <89126668+pulala17@users.noreply.github.com>
2022-03-21 16:46:52 +0800

    Update 2_data_types.sv

--- 2_data_types.sv
+++ 2_data_types.sv
@@ -85,6 +85,178 @@ end
 // a 'foreach(f[i])' is equivalent to 'for(int i=O; i<=4; i++)'. 
 // With the array 'rev[6:2]', the statement 'foreach (rev[i])' is equivalent to 'for(int i=6; i>=2; i--)'
 
+// Sample 2.15 Packed array declaration and usage
+bit [3:0] [7:0] bytes;      // 4 bytes packed into 32-bits
+bytes = 32'hCafe_Dada;
+$displayh(bytes,,           // Show all 32-bits
+          bytes [3],,       // Most significant byte "CA"
+          bytes [3] [7] ) ; // Most significant bit "I"
+//
+//         | bytes [3]| bytes [2]| bytes [1]| bytes [0]| 
+// bytes : | 76543210 | 76543210 | 76543210 | 76543210 |
+
+// Sample 2.16 Declaration for a mixed packed/unpacked array
+bit [3:0] [7:0] barray [3];    // Packed: 3x32-bit
+bit [31:0] lw = 32'h0123 4567; // Word
+bi t [7:0] [3:0] nibbles; //Packed array of nibbles
+barray[0] = lw;
+barray[0][3] = 8'h01;
+barray[0][1][6] = 1'b1;
+nibbles = barray[0];      // Copy packed values
+
+//            | b [0][3] | b [0][2] | b [0][1] | b [0][0] |
+// barray [0] | 76543210 | 76543210 | 76543210 | 76543210 |
+// barray [1] | 76543210 | 76543210 | 76543210 | 76543210 |
+// barray [2] | 76543210 | 76543210 | 76543210 | 76543210 |
+
+// Note: because one dimension is specified after the name,
+// barray [3] , that dimension is unpacked,
+
+// A packed array is handy if u need to convert to and from scalars. 
+// For example, you might need to reference a memory as a byte or as a word.
+// If you need to wait for a change in an array, you have to use a packed array. 
+// Perhaps your testbench might need to wake up when a memory changes value, and so you want to use the @operator.
+// This is however only legal with scalar values and packed arrays.
+
+------------------------------------------------------------------------------------------------------------------------
+// Sample 2.17 Using dynamic arrays
+int dyn[], d2[]; // Declare dynamic arrays
+initial begin
+  dyn = new[5];                 // A: Allocate 5 elements
+  foreach (dyn[j]) dyn[j] = j;  // B: Initialize the elements
+  d2 = dyn;                     // C: Copy a dynamic array
+  d2[0] = 5;                    // D: Modify the copy
+  $display (dyn[0], d2[0]);     // E: See both values (0 & 5)
+  dyn = new[20] (dyn);          // F: Allocate 20 ints 
+                                // & copy the existing 5 elements of dyn to the beginning of the array
+  dyn = new[100];               // G: Allocate 100 new ints.   Old values are lost
+  dyn.delete();                 // H: Delete all elements
+end
+
+// Sample 2.18 Using a dynamic array for an uncounted list
+bit [7:0] mask[] = '{8'b0000_0000, 8'b0000_0001,
+                     8'b0000_0011, 8'b0000_0111,
+                     8'b0000_2111, 8'b0001_1111,
+                     8'b0011_1111, 8'b0111 1111,
+                     8'b1111_1111} ;
+// there are 9 masks for 8 bits, can let SV coungt them, 
+// rather than making a fixed-size array adn accidently choosing the wrong size of other numbers
+
+// dynamic array can be assigned to a fixed array (when they have same number of elements)
+
+------------------------------------------------------------------------------------------
+[QUEUE]
+// A queue is declared with [$]
+// the elements of a queue are numbered from 0 to $
+
+// Sample 2.19 Queue operations
+int j = 1,
+q2[$] = {3, 4},     // queue literals do not use '
+q[$]  = {0, 2, 5};
+initial begin
+  q.insert(1, j);   // insert 1 before 2
+  q.insert(3, q2);  // insert queue in q1
+  q.delete(1);      // delete elem. #1
+  
+  // these operation are fast
+  q.push_front(6);  // insert at front
+  j = q.pop_back;   // j = 5
+  q.push_back(8);   // insert at back
+  j = q.pop_front;  // j = 6
+  foreach (q[i])
+    $display (q[i]);  // print entire queue
+  q.delete() ;        // delete entire queue
+end
+
+// Note that queue literals only have curly braces, 
+// and are missing the initial apostrophe of array literals
+
+// if $ on the left side eg.[$:2],  $ stands for the min. value, [0:2]
+// if $ on the right side eg.[3:$], $ stands for the max. value
+
+// Sample 2.20 Queue operations
+int j = 1,
+q2[$] = {3, 4}, 
+q[$] = {0,2,5};
+initial begin
+  q = {q[0], j, q[1:$]};    // {0,1,2,5} insert 1 before 2
+  q = {q[0:2], q2, q[3:$]}; // {0,1,2,3,4,5} insert queue in q
+  q = {q[0], q[2:$]};       // {0,2,3,4,5} delete elem. #1
+
+  // these operations are fast
+  q = {6,q};    // {6,0,2,3,4,5} insert at front
+  j = q[$];     // j = 5
+  q = q[0:$-1]; // {6,0,2,3,4} pop_back
+  q = {q,8};    // {6,0,2,3,4,8}  insert at back
+  j = q[0];     // j = 6
+  q = q[1:$];   // {0,2,3,4,8} pop_front
+  q = {};       // delete entire queue
+end
+
+---------------------------------------------------------------------------------
+[ ASSOCIATIVE ARRAYS ]
+// When modeling a processor that has a multi-gigabyte address range. 
+// During a typical test, the processor may only touch a few hundred or thousand memory locations 
+// which containing executable code and data, so allocating and initializing gigabytes of storage is wasteful.
+//
+// SV offers associative arrays to store entries in a sparse matrix
+
+// Sample 2.21 Declaring. initializing. and using associative arrays
+initial begin
+  bit [63:0] assoc[int], idx = 1;
+  
+  // Initialize widely scattered values
+  repeat (64) begin
+    assoc[idx] = idx;
+    idx = idx << 1;
+  end
+  
+  // Step through all index values with foreach
+  foreach (assoc[i])
+    $ display (Rassoc [%h] = %h", i, assoc[i]);
+               
+  // Step through all index values with functions
+  if (assoc.first(idx)) begin
+    do
+      $display ("assoc[%h] = %h", idx, assoc[idx]);
+    while (assoc.next(idx)); // Get next index
+  end
+  // Find and delete the first element
+  assoc.first(idx);
+  assoc.delete(idx) ;
+  $display("The array now has %0d elements", assoc.num);
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
